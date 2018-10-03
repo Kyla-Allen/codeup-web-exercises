736ee840ce260d2af949fe242b122df9cce3e465
Kyla Allen <kja.kyla.allen@gmail.com>
2018-10-03 14:18:24 -0500

    added functions.js and functions_js.html

---
+++ js/functions-lecture-exercises.js
@@ -0,0 +1,31 @@
+"use strict";
+
+//solution 1
+//     var hasLetterA = function (input){
+//         var hasLetterA = false;
+//         if (input.includes("A") || input.includes("a")) {
+//             hasLetterA = true;
+//         }
+//         return hasLetterA;
+//     }
+
+
+//solution 2
+    //This function goes through the string and sets theLetterA as true if there's an a
+    // var hasLetterA = function (input) {
+    //     var theLetterA = false;
+    //     var lengthOfString = input.length;
+    //
+    //     for (var count = 0; count <= lengthOfString; count ++){
+    //         if (input.charAt(count) === "a" || input.charAt(count)=== "A" )
+    //         {
+    //             theLetterA = true;
+    //         }
+    //     }
+    //
+    //     return theLetterA;
+    // }
+
+
+    //print out result
+    //console.log (hasLetterA('wooo'));
---
+++ js/functions.js
@@ -0,0 +1,123 @@
+"use strict";
+
+/**
+ * TODO:
+ * Create a function called 'sayHello' that takes a parameter 'name'.
+ * When called, the function should return a message that says hello to the passed in name.
+ *
+ * Example
+ * > sayHello("codeup") // returns "Hello, codeup!"
+ */
+
+// var sayHello = function (name) {
+//     var message = "Hello, " + name + "!";
+//     return message;
+// }
+
+
+/**
+ * TODO:
+ * Call the function 'sayHello' and pass your name as a string literal argument.
+ * Store the result of the function call in a variable named 'helloMessage'.
+ *
+ * console.log 'helloMessage' to check your work
+ */
+
+// var helloMessage =sayHello("Kyla the Amazing");
+// console.log(helloMessage);
+
+/**
+ * TODO:
+ * Store your name as a string in a variable named 'myName', and pass that
+ * variable to the 'sayHello' function. You should see the same output in the
+ * console.
+ */
+
+//
+// var myName = "Kyla The Superbly Amazing";
+// console.log(sayHello(myName));
+
+
+
+// Don't modify the following line, it generates a random number between 1 and 3
+// and stores it in a variable named random
+var random = Math.floor((Math.random() * 3) + 1);
+
+/**
+ * TODO:
+ * Create a function called 'isTwo' that takes a number as a parameter.
+ * The function should return a boolean value based on whether or not the passed
+ * number is the number 2.
+ *
+ * Example
+ * > isTwo(1) // returns false
+ * > isTwo(2) // returns true
+ * > isTwo(3) // returns false
+ *
+ * Call the function 'isTwo' passing the variable 'random' as a argument.
+ *
+ * console.log *outside of the function* to check your work (you should see a
+ * different result everytime you refresh the page if you are using the random
+ * number)
+ */
+
+// var isTwo = function (number) {
+//     butIsitTwo = false;
+//     if(number === 2){
+//         var butIsitTwo = true;
+//     }
+//     return butIsitTwo;
+// }
+// console.log(isTwo(random));
+
+
+/**
+ * TODO:
+ * Create a function named 'calculateTip' to calculate a tip on a bill at a
+ * restaurant. The function should accept a tip percentage and the total of the
+ * bill, and return the amount to tip
+ *
+ * Examples:
+ * > calculateTip(0.20, 20) // returns 4
+ * > calculateTip(0.25, 25.50) // returns 6.37
+ * > calculateTip(0.15, 33.42) // returns 5.013
+ */
+// var calculateTip = function (percentage, total_bill) {
+//     var tip = percentage * total_bill;
+//     tip = tip.toFixed(2);
+//     return tip;
+// }
+/**
+ * TODO:
+ * Use prompt and alert in combination with your calculateTip function to
+ * prompt the user for the bill total and a percentage they would like to tip,
+ * then display the dollar amount they should tip
+ */
+// var usersTotalBill = parseFloat(prompt("How much is the total bill?"));
+// var usersPercentageTipped = parseFloat(prompt("What percentage would you like to tip?"));
+//
+// alert("The tip is $" + calculateTip(usersPercentageTipped, usersTotalBill));
+
+
+
+
+/**
+ * TODO:
+ * Create a function named `applyDiscount`. This function should accept a price
+ * (before a discount is applied), and a discount percentage (a number between 0
+ * and 1). It should return the result of applying the discount to the original
+ * price.
+ *
+ * Example:
+ * > var originalPrice = 100;
+ * > var dicountPercent = .2; // 20%
+ * > applyDiscount(originalPrice, dicountPercent) // 80
+ *
+ * > applyDiscount(45.99, 0.12) // 40.4712
+ */
+// var applyDiscount = function (originalPrice, discountPercent) {
+//     var finalPriceAfterDscnt = 0;
+//     finalPriceAfterDscnt = originalPrice - (originalPrice * discountPercent);
+//     return finalPriceAfterDscnt;
+// }
+// console.log (applyDiscount(100, .2));
