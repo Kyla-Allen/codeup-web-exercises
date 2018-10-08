3bed03c416a1e7f98767f0b3613c0df551c13583
Kyla Allen <kja.kyla.allen@gmail.com>
2018-10-08 16:38:35 -0500

    began loop exercises

---
+++ js/while.js
@@ -0,0 +1,15 @@
+var number = 2;
+
+
+//while loop
+while (number <= 65536){
+    console.log(number);
+     number = number *2;
+}
+
+
+//do-while loop
+do {
+    console.log(number);
+    number = number * 2;
+} while (number <= 65536);
