18b876a194bf2571cce53cdc2eab7f2d77244011
Kyla Allen <kja.kyla.allen@gmail.com>
2018-10-08 16:55:41 -0500

    finished the while loop exercises

--- js/while.js
+++ js/while.js
@@ -1,15 +1,34 @@
-var number = 2;
+// var number = 2;
+//
+//
+// //while loop
+// while (number <= 65536){
+//     console.log(number);
+//      number = number *2;
+// }
+//
+//
+// //do-while loop
+// do {
+//     console.log(number);
+//     number = number * 2;
+// } while (number <= 65536);
 
 
-//while loop
-while (number <= 65536){
-    console.log(number);
-     number = number *2;
+
+// This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+while (allCones > 0){
+// This expression will generate a random number between 1 and 5
+    var conesBought = Math.floor(Math.random() * 5) + 1;
+    if (allCones - conesBought < 0){
+        console.log("I do not have " + conesBought + " cones, I only have " + allCones);
+    } else {
+        console.log("I sold " + conesBought);
+       allCones = allCones - conesBought;
+       console.log("I have " + allCones + " left");
+    }
+
+
 }
-
-
-//do-while loop
-do {
-    console.log(number);
-    number = number * 2;
-} while (number <= 65536);
