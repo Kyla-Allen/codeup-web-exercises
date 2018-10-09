34d379bd4809dfdf48cc4fffb882f89d35fbf35b
Kyla Allen <kja.kyla.allen@gmail.com>
2018-10-09 11:01:07 -0500

    complete the for_loops and while exercises

--- js/while.js
+++ js/while.js
@@ -16,19 +16,23 @@
 
 
 
-// This is how you get a random number between 50 and 100
-var allCones = Math.floor(Math.random() * 50) + 50;
-
-while (allCones > 0){
-// This expression will generate a random number between 1 and 5
-    var conesBought = Math.floor(Math.random() * 5) + 1;
-    if (allCones - conesBought < 0){
-        console.log("I do not have " + conesBought + " cones, I only have " + allCones);
-    } else {
-        console.log("I sold " + conesBought);
-       allCones = allCones - conesBought;
-       console.log("I have " + allCones + " left");
-    }
-
-
-}
+// // This is how you get a random number between 50 and 100
+// var allCones = Math.floor(Math.random() * 50) + 50;
+//
+// while (allCones > 0){
+// // This expression will generate a random number between 1 and 5
+//     var conesBought = Math.floor(Math.random() * 5) + 1;
+//     if (allCones - conesBought < 0){
+//         console.log("I do not have " + conesBought + " cones, I only have " + allCones);
+//     }
+//     else {
+//         console.log("I sold " + conesBought);
+//        allCones = allCones - conesBought;
+//        console.log("I have " + allCones + " left");
+//        if (allCones === 0){
+//            console.log("Yay! I sold all the cones!");
+//        }
+//     }
+//
+//
+// }
