364a73f9da42d1d7ad098821122c70ec873c0643
Kyla Allen <kja.kyla.allen@gmail.com>
2018-10-10 15:25:24 -0500

    added the array bonuses. not done with those yet. started the planet-array exercise

---
+++ js/array_bonuses.js
@@ -0,0 +1,49 @@
+// ================================= ARRAY BONUSES
+// 1. Create a function that returns a random day of the week
+
+// var daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+// function returnRandomElement (array){
+//     return array[Math.floor((Math.random() * 6))];
+// }
+// console.log(returnRandomElement(daysOfTheWeek));
+
+
+// 2. Create a function that takes a single letter and returns what number the letter is in the alphabet. Ignore case.
+// someFunction("a") // returns 1
+// someFunction("z") // returns 26
+
+// var alphabet = ["a","b","c","d","e","f", "g","h","i","j","k","l","m","n", "o", "p", "q"
+// ,"r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// function getPlace (array, letter) {
+//     letter = letter.toLowerCase();
+//     return array.indexOf(letter) + 1;
+// }
+// console.log(getPlace(alphabet, "D"));
+
+
+// 3. Create a function that returns the longest string in a given array of string elements
+
+// var strings = ["LALALA", "Once there was an angry platypus", "moo"];
+// function findLongestString (array){
+//     var longest = "";
+//     array.forEach(function (element) {
+//         if(element.length > longest.length){
+//             longest = element;
+//         }
+//     })
+//     return longest;
+// }
+// console.log(findLongestString(strings));
+
+
+// 4. Create a function that takes in two arrays of number inputs. If all numbers added together
+// in the first array is equal to all the number inputs added in the second array, return true,
+// otherwise, false. Only add together numeric elements but either array may contain non-numeric elements.
+//     var arr1 = ['bob', 1, true, 1, 2];
+// var arr2 = [2, null, undefined, 0, 2, "apple"]
+// exampleFunction(arr1, arr2) // returns true
+
+
+var numberGroup1 = [1, 2, 3];
+var numberGroup2 = [2, 1, 3];
+
