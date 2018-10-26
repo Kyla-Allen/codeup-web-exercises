bf07e293ee59cca0d6958f6dc89ec522d8140772
Kyla Allen <kja.kyla.allen@gmail.com>
2018-10-26 11:58:53 -0500

    working with local storage

--- js/ajax-blog.js
+++ js/ajax-blog.js
@@ -11,7 +11,11 @@ function update (){
     posts.fail(function (){
         alert("ERROR");
     })
+
 }
+
+
+
 
 //building html....
 function buildHTML (data){
@@ -31,7 +35,55 @@ function buildHTML (data){
     return html;
 }
 
+//from local storage
+function buildHTMLfromLocal (data){
+    var html = '';
+        html += "<div class='card m-1'>";
+        html += "<h1 class='card-title m-1'>" + data.title + "</h1>";
+        html += "<p class='card-text m-2'>" + data.content + "</p>";
+        html += "<ul>";
+        data.categories.forEach(function (category) {
+         html += "<li>" + category + " </li>";
+        })
+
+        html += "</ul>";
+        html += "<h6 class='m-2'>" + data.date + "</h6>";
+        html += "</div>";
+    return html;
+}
+
+//EXTRA STUFF
+
+// We gotta get the input from the form
+function getPost(){
+    var d = new Date();
+    var post ={
+        "title": $(".title").val(),
+        "content": $(".post").val(),
+        "categories": getTags(),
+        "date": d
+    };
+    return post;
+}
+
+function getTags(){
+    var tags = $(".tags").val().split(",");
+    return tags;
+}
+
+
+// we add that form as an object to local storage
+$(".postBtn").on('click', function (e) {
+    console.log(getPost());
+    localStorage.setItem("posts", JSON.stringify(getPost()));
+});
+
+
+// Then, load the homepage and the post should be there!
+
+
+//THIS STUFF NEEDS TO EXIST
 update();
 $("button").on('click', function (e) {
-    update();
-})
+        update();
+});
